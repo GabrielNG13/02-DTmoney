d1e5b5c4a2311e9164de6fe2cce2b3e2b2507d5a
GabrielNG13 <gabrielnunesmghirardelli@gmail.com>
2023-01-18 21:19:28 -0300

    MirageJS config

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs'
 import { App } from './App';
+import { timingSafeEqual } from 'crypto';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Salário',
+          amount: 1400,
+          category: 'Renda',
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Compras',
+          amount: 300,
+          category: 'Gasto',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
