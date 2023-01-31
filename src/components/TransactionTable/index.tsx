b3f3e1cc78dad2b4fe5effd640e2842a8c42d65d
GabrielNG13 <gabrielnunesmghirardelli@gmail.com>
2023-01-30 21:48:02 -0300

    Seeds and transaction listing

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+    id: number
+    title: string
+    amount: number
+    type: string
+    category: string
+    createdAt: string
+}
+
 export function TransactionTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
         api.get('/transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
     }, []);
     
     return (
@@ -21,18 +32,14 @@ export function TransactionTable() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="income">R$12.000</td>
-                        <td>Serviço</td>
-                        <td>18/01/2023</td>
-                    </tr>
-                    <tr>
-                        <td>Lanche</td>
-                        <td className="outcome">R$60</td>
-                        <td>Alimentação</td>
-                        <td>18/01/2023</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>{transaction.amount}</td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createdAt}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+      server.db.loadData({
+        transactions: [
+          {
+            id: 1,
+            title: 'Salário',
+            type: 'deposit',
+            category: 'Renda',
+            amount: 1419.26,
+            createdAt: new Date('2022-06-05 10:15:45') 
+          },
+          {
+            id: 2,
+            title: 'Aluguel',
+            type: 'withdraw',
+            category: 'Moradia',
+            amount: 800,
+            createdAt: new Date('2022-06-08 10:15:45') 
+          }
+        ]
+      })
+  },
+
   routes() {
     this.namespace = 'api';
 
